b61d4d450778f3cf4ba6e9103f34815948c7d6aa
mariacuffaro <maria.cuffaro@johnlewis.co.uk>
2020-12-06 13:10:39 +0000

    adding working part 2

--- day-six/daySix.js
+++ day-six/daySix.js
@@ -1,12 +1,9 @@
 // read in passports
 var fs = require('fs');
 var input = fs.readFileSync("./input.txt", 'utf8');
-// var input = fs.readFileSync("./testInvalidInput.txt", 'utf8');
-// var input = fs.readFileSync("./testInput.txt", 'utf8');
+
 //create an array of rows
 var groupAnswers = input.split(/\r?\n{2,}/);
-// console.log('groupanswers', groupAnswers);
-
 
 const uniqueValues = (string) =>{
     const array = string.split('')
@@ -22,65 +19,37 @@ const uniqueValues = (string) =>{
 }
 
 // console.log('test unique', uniqueValues('aaabs'));
-let groupArrays = []
+let groups = []
+let uniqueAnswers = [];
 
-let withoutDuplicates = [];
 groupAnswers.forEach(group => {
-    // console.log('here', group);
-    // console.log('group', group);
-
-    groupArrays.push(group.split('\n'));
-
-    const removeDuplicates = uniqueValues(group)
-    withoutDuplicates.push(removeDuplicates.replace(/\r?\n|\r/g, '')); 
-
+    groups.push(group.split('\n'));
+    uniqueAnswers.push(uniqueValues(group).replace(/\r?\n|\r/g, '')); 
 });
 
-
-
-let count = 0;
+let partOneCount = 0;
+let partTwoCount = 0;
 let i = 0
-withoutDuplicates.forEach(group => {
-    console.log('group', group);
-    console.log('group array', groupArrays[i]);
-    let numberInGroup =  groupArrays[i].length
-    console.log('numberInGroup', numberInGroup);
 
+uniqueAnswers.forEach(group => {
+    partOneCount += group.length
+    const currentGroup = groups[i]
+    const numberInGroup =  currentGroup.length
     const answers = group.split('')
-    const currentGroup = groupArrays[i]
+
     answers.forEach(answer => {
-        console.log('answer', answer);
         let answerCount = 0
         currentGroup.forEach(personInGroup =>{
-            console.log('person', personInGroup);
             if (personInGroup.includes(answer)){
                 answerCount++
             }
         })
-        console.log('answerCount', answerCount);
-
         if (answerCount === numberInGroup){
-            count ++
+            partTwoCount ++
         }
     })
-    console.log('count', count);
-
-    
-// console.log('group', group);
 i ++
 });
-console.log(count);
+console.log(partOneCount);
+console.log(partTwoCount);
 
-//part 2
-// You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
-// In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
-
-// In the second group, there is no question to which everyone answered "yes".
-// In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
-// In the fourth group, everyone answered yes to only 1 question, a.
-// In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
-
-//for each group
-//if number in group = 1 questions all answered yes to = length of group 
-let questionCount = 0
-// for
